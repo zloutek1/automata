3d89b643e23eb96cb0dd3b79658eab1ec8879ca5
Tomáš Ljutenko <xljutenk@fi.muni.cz>
2019-11-16 21:56:08 +0100

    testing script with example automatas

--- main.py
+++ main.py
@@ -1,367 +1,116 @@
-from itertools import *
-import functools
-import operator
-
-
-class GenCacher:
-    def __init__(self, generator):
-        self._g = iter(generator)
-        self._cache = []
-
-    def __getitem__(self, idx):
-        while len(self._cache) <= idx:
-            self._cache.append(next(self._g))
-        return self._cache[idx]
-
-
-"""
-class Iterace:
-    def __init__(self, jazyk, _from=0):
-        self._args = None
-        self.start_from = _from
-
-        if isinstance(jazyk, Jazyk):
-            self.jazyk = jazyk
-            self._iter_args = self.args
-            return
-
-        if isinstance(jazyk, Iterace):
-            self.jazyk = jazyk
-            self._iter_args = self.args
-            return
-
-        self._args = jazyk
-
-    @property
-    def args(self):
-        if self._args:
-            return self._args
-        return map(lambda arg: Slovo("".join(map(repr, arg))), chain.from_iterable(product(self.jazyk, repeat=i) for i in count(self.start_from)))
-
-    def __iter__(self):
-        return iter(self.args)
-
-    def __mul__(self, _other):
-        if isinstance(_other, Iterace):
-            return Iterace(self.product(self, _other))
-
-        if isinstance(_other, Jazyk):
-            return Iterace(chain.from_iterable(map(lambda slovo: [
-                slovo * _other_slovo for _other_slovo in _other], self)))
-
-        raise NotImplementedError()
-
-    def __pow__(self, to):
-        if isinstance(to, int):
-            if to <= 0:
-                return Jazyk(Slovo(""))
-            return self * self ** (to - 1)
-
-        if to == "*":
-            return Iterace(self.args)
-
-        raise NotImplementedError()
-
-    def __repr__(self):
-        slova = str(sorted(islice(self.args, 10), key=abs))
-        return f"{{{slova[1:-1]}, ...}}"
-
-    @staticmethod
-    def product(gen1, gen2):
-        gc1 = GenCacher(gen1)
-        gc2 = GenCacher(gen2)
-        idx1 = idx2 = 0
-        moving_up = True
-
-        while True:
-            yield (gc1[idx1] * gc2[idx2])
-
-            if moving_up and idx1 == 0:
-                idx2 += 1
-                moving_up = False
-            elif not moving_up and idx2 == 0:
-                idx1 += 1
-                moving_up = True
-            elif moving_up:
-                idx1, idx2 = idx1 - 1, idx2 + 1
-            else:
-                idx1, idx2 = idx1 + 1, idx2 - 1
-"""
-
-
-class Iterace:
-    def __init__(self, val, from_power=0):
-        self.from_power = from_power
-        self._args = None
-
-        if isinstance(val, Jazyk):
-            self.val = val
-            return
-
-        self._args = val
-
-    @property
-    def args(self):
-        if self._args:
-            return self._args
-        return self._get_args()
-
-    def _get_args(self):
-        return chain.from_iterable(
-            sorted(
-                map(lambda arg:
-                    Slovo(
-                        "".join(map(repr, arg))
-                    ),
-                    product(self.val, repeat=i)
-                    ),
-                key=lambda slovo: (abs(slovo), slovo)
-            )
-            for i in count(self.from_power)
-        )
-
-    def __mul__(self, _other):
-        if isinstance(_other, Jazyk):
-            return Iterace(chain.from_iterable(map(lambda slovo: [
-                slovo * _other_slovo for _other_slovo in _other], self)))
-
-    def __pow__(self, to):
-        if isinstance(to, int):
-            if to <= 0:
-                return Jazyk(Slovo(""))
-            return self * self ** (to - 1)
-
-        if to == "*":
-            return Iterace(self)
-
-    def __iter__(self):
-        return iter(self.args)
-
-    def __repr__(self):
-        slova = str(list(islice(self.args, 10)))
-        return f"{{{slova[1:-1]}, ...}}"
-
-
-class Slovo:
-    def __init__(self, val, *, Σ=None):
-        if not Σ:
-            self.Σ = globals().get("Σ")
-        else:
-            self.Σ = Σ
-
-        if isinstance(val, str):
-            self.val = val
-            assert self.check_buildable(
-                self.val, self.Σ), f"Slovo {self.val} nepatri do abecedy Σ"
-            return
-
-        if isinstance(val, Slovo):
-            self.val = val.val
-            assert self.check_buildable(
-                self.val, self.Σ), f"Slovo {self.val} nepatri do abecedy Σ"
-            return
-
-        raise TypeError("unsuported type for val in slovo")
-
-    def check_buildable(self, slovo, from_Σ):
-        buildable = False
-        if from_Σ:
-            for hlaska in from_Σ:
-                if hlaska == slovo[:abs(hlaska)]:
-                    buildable = True
-                    buildable &= self.check_buildable(
-                        slovo[abs(hlaska):], from_Σ)
-        if not from_Σ or len(slovo) == 0:
-            return True
-        return buildable
-
-    def 井(self, _of):
-        if isinstance(_of, str):
-            return self.val.count(_of)
-
-    def __pow__(self, to):
-        if to == "R":
-            return Slovo(self.val[::-1])
-
-        elif isinstance(to, int):
-            return Slovo(self.val * to)
-
-    def __mul__(self, _with):
-        if isinstance(_with, Slovo):
-            return Slovo(self.val + _with.val)
-
-    def __eq__(self, _with):
-        if isinstance(_with, Slovo):
-            return repr(self) == repr(_with)
-
-        if isinstance(_with, str):
-            if self.val == "" and _with == "Ɛ":
-                return True
-            return self.val == _with
+from utils import Set
+import FA
+import DFA
+import NFA
+import EFA
+import RE
+import CFG
+import G
+
+
+def test_dfa():
+    δ = DFA.δ()
+    A = DFA(Set(1, 2, 3, 4, 5, 6, 7), Set("a", "b"), δ, 1, Set(3, 5, 6))
+
+    δ[1, "a", "b"] = 2, "-"
+    δ[2, "a", "b"] = 3, 4
+    δ[3, "a", "b"] = 6, 5
+    δ[4, "a", "b"] = 3, 2
+    δ[5, "a", "b"] = 6, 3
+    δ[6, "a", "b"] = 2, "-"
+    δ[7, "a", "b"] = 6, 1
+
+    # A.table()
+    B = A.minimize()
+    # B.table()
+
+    ######################################################################
+
+    δ = DFA.δ()
+    A = DFA(Set("r1", "r2", "p", "q1", "q2"), Set(
+        "F1", "F2", "E", "G1", "G2"), δ, "r1", Set("q2"))
+
+    δ["r1", "F1"] = "p"
+    δ["r2", "F2"] = "p"
+    δ["p", "E"] = "p"
+    δ["p", "G1"] = "q1"
+    δ["p", "G2"] = "q2"
+
+    A.diagram()
+    B = A.toRE()
+    B.diagram()
+
+
+def test_nfa():
+    δ = NFA.δ()
+    A = NFA(Set(1, 2, 3, 4, 5), Set("a", "b", "c", "d"), δ, 2, Set(4))
+
+    δ[1, "a", "b", "c", "d"] = Set(), Set(4), Set(), Set(1)
+    δ[2, "a", "b", "c", "d"] = Set(), Set(4), Set(2, 5), Set()
+    δ[3, "a", "b", "c", "d"] = Set(1), Set(1, 3), Set(1, 2), Set(1, 2)
+    δ[4, "a", "b", "c", "d"] = Set(), Set(3, 4), Set(), Set()
+    δ[5, "a", "b", "c", "d"] = Set(4), Set(1, 4), Set(4), Set()
+
+    B = A.toDFA()
+    # B.table()
+
+
+def test_efa():
+    δ = EFA.δ()
+    A = EFA(Set(0, 1, 2, 3, 4), Set("a", "b", "c", "d"), δ, 0, Set(2))
+
+    δ[0, "a", "b", "c", "d", "ε"] = Set(), Set(), Set(), Set(), Set(1)
+    δ[1, "a", "b", "c", "d", "ε"] = Set(0), Set(), Set(), Set(3), Set(2)
+    δ[2, "a", "b", "c", "d", "ε"] = Set(3), Set(), Set(), Set(), Set()
+    δ[3, "a", "b", "c", "d", "ε"] = Set(), Set(), Set(), Set(4), Set(4)
+    δ[4, "a", "b", "c", "d", "ε"] = Set(), Set(3), Set(2), Set(), Set()
+
+    B = A.toNFA()
+    C = A.toDFA()
+    # B.table()
+
+
+def test_re():
+    δ = RE.δ()
+    A = RE(Set(0, 1), Set(), δ, Set(0), Set(1))
+    δ[0, "a.((a+b)*+c).d"] = 1
+    B = A.toEFA()
+    # B.diagram()
+
+
+def test_grammar():
+    P = G.P({
+        "S": "aA | bC | a | ε",
+        "A": "bB | aA | b | c",
+        "B": "aB | bC | aC | cA | c",
+        "C": "a | b | aA | bB"
+    })
+    A = G(Set("S", "A", "C", "B"), Set("a", "b", "c"), P, "S")
+    B = A.toNFA()
+    # B.table()
+
+
+def test_cfg():
+    P = CFG.P({
+        "A": "Bd | c",
+        "B": "Bdd | Ccc | aCd",
+        "C": "Aa"
+    })
+    A = CFG(Set("A", "B", "C"), Set("a", "b", "c", "d"), P, "A")
+    A.Ne()
+
 
-    def __lt__(self, _with):
-        if isinstance(_with, Slovo):
-            return self.val < _with.val
+def test():
+    simpleregex = "3+(4+(5+[6+(7+8)+9]+10)+11)+12"
+    A = EFA.EFA.fromRegex(simpleregex)
+    print(A)
+    A.diagram()
 
-    def __abs__(self):
-        return len(self.val)
 
-    def __hash__(self):
-        return hash(self.val)
-
-    def __repr__(self):
-        if len(self.val) == 0:
-            return "Ɛ"
-        return str(self.val)
-
-
-def 井(_of, _in):
-    return _in.井(_of)
-
-
-R = "R"
-
-
-class Jazyk:
-    def __init__(self, *args):
-        args = list(args)
-
-        if isinstance(args[0], tuple):
-            self.args = set(args[0])
-            return
-
-        if isinstance(args[0], set):
-            self.args = args[0]
-            return
-
-        if isinstance(args[0], Jazyk):
-            self.args = args[0].args
-            return
-
-        for i, arg in enumerate(args):
-            if isinstance(arg, str):
-                args[i] = Slovo(arg)
-
-        self.args = set(args)
-
-    def __and__(self, _other):
-        if isinstance(_other, Jazyk):
-            return Jazyk(set(self.args) & set(_other.args))
-
-    def __or__(self, _other):
-        if isinstance(_other, Jazyk):
-            return Jazyk(set(self.args) | set(_other.args))
-
-    def __sub__(self, _other):
-        if isinstance(_other, Jazyk):
-            return Jazyk(set(self.args) - set(_other.args))
-
-    def __mul__(self, _other):
-        if isinstance(_other, Jazyk):
-            return Jazyk(set(u * v for u in self.args for v in _other.args))
-
-        elif isinstance(_other, Iterace):
-            return _other * self
-
-    def __pow__(self, to):
-        if isinstance(to, int):
-            if to <= 0:
-                return Jazyk(Slovo(""))
-            return Jazyk(self * self ** (to - 1))
-
-        elif to == "*":
-            return Iterace(self)
-
-        elif to == "+":
-            return Iterace(self, 1)
-
-    def __eq__(self, _other):
-        if isinstance(_other, Jazyk):
-            return repr(self) == repr(_other)
-
-        if isinstance(_other, set):
-            return self.args == _other
-
-    def __getitem__(self, key):
-        return sorted(self.args)[key]
-
-    def __abs__(self):
-        return len(self.args)
-
-    def __iter__(self):
-        return iter(sorted(self.args))
-
-    def __repr__(self):
-        return f"{{{str(sorted(self.args))[1:-1]}}}"
-
-
-class Abeceda:
-    def __init__(self, *args):
-        args = list(args)
-
-        for i, arg in enumerate(args):
-            if isinstance(arg, str):
-                args[i] = Slovo(arg)
-
-        self.args = args
-
-    def __pow__(self, to):
-        return Jazyk(*self.args) ** to
-
-    def __iter__(self):
-        return iter(list(self.args))
-
-    def __repr__(self):
-        return f"{{{str(sorted(self.args))[1:-1]}}}"
-
-#
-#
-# Code here
-
-
-"""
-u = Slovo("abba")
-v = Slovo("ca")
-
-assert u * v == "abbaca"
-assert abs(u) == 4
-assert 井("b", u) == 2
-assert u ** 2 == "abbaabba"
-assert u ** 3 == "abbaabbaabba"
-assert u ** 0 == ""
-assert u ** 0 == "Ɛ"
-assert v ** R == "ac"
-
-L1 = Jazyk(Slovo("xy"), Slovo("y"), Slovo("yx"))
-L2 = Jazyk(Slovo("y"), Slovo("z"))
-
-assert L1 & L2 == {"y"}
-assert L1 | L2 == {"xy", "y", "yx", "z"}
-assert L1 - L2 == {"xy", "yx"}
-assert L1 * L2 == {"xyy", "yy", "yxy", "xyz", "yz", "yxz"}
-
-assert L1 ** 0 == {""}
-assert L1 ** 1 == L1
-assert L1 ** 2 == {"yxxy", "yxy", "xyxy", "xyyx", "yyx", "yy", "xyy", "yxyx"}
-
-L1 = Jazyk("a")**"*"
-L2 = Jazyk("b")**"*"
-print(L1 * L2)
-
-Σ = Abeceda("ab", "bc")
-print((Σ ** "*"))
-"""
-
-print((Slovo("bab")**R * Slovo("ba")**R)**2)
-
-L1 = Jazyk("ba", "ab", "")
-L2 = Jazyk("b")
-L3 = Jazyk("a", "")
-
-a = L1 * L2
-b = a ** "*"
-c = b * L3
-print(type(a), a)
-print(type(b), b)
-print(type(c), c)
-print(c ** 1)
+test_dfa()
+test_nfa()
+test_efa()
+test_re()
+test_grammar()
+test_cfg()
+# test()
